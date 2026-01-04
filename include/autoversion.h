9123334df462ec3323e7b68ca5af38d50c3e0a5e
byteWarden <wardenbyte@gmail.com>
2026-01-04 02:33:59 +0100

    Initial commit

---
+++ include/autoversion.h
@@ -0,0 +1,15 @@
+#ifndef __AUTOVERSION_H__
+#define __AUTOVERSION_HPP__
+
+/// Firmware versioning
+#define FIRMWARE_MAJOR_VERSION 3
+#define FIRMWARE_MINOR_VERSION 4
+#define FIRMWARE_PATCH_VERSION 3613
+
+/// Firmware version string
+#define FIRMWARE_VERSION "3.4.3613"
+
+
+
+#endif // __AUTOVERSION_H__
+
---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <stdafx.h>
+
+
+// v3.0.2991
+void setup()
+{
+	Wire.begin();  				// Start I2C as master
+	Serial.begin(9600);			// Initialize serial communication at 9600 bits per second
+}	
+
+
+void loop() 
+{
+
+}	
